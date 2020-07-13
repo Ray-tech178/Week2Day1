2cfe607e86cf0a01406286dbf289011f6d498264
Ray-tech178 <67976062+Ray-tech178@users.noreply.github.com>
2020-07-13 14:42:22 -0500

    Add files via upload

---
+++ Raymond_L_operator exercise.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+///top section
+let valueOne = 1
+let valueTwo = 2
+//middle section
+let addition = valueOne + valueTwo
+let subtracton = valueOne - valueTwo
+let multiplication = valueOne * valueTwo
+let division = valueOne / valueTwo
+//bottom section
+print(addition)
+print(subtracton)
+print(multiplication)
+print(division)
